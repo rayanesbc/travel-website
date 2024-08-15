436455f9b25b37f375a659b52ff80634e6a73e21
Rayane <rayanebarros@outlook.com>
2024-08-15 00:51:55 -0300

    Top Destination Final, Swiper2 e Responsivo do Choose Your Place, Counter Numbers e Popular Destination

--- js/swiper.js
+++ js/swiper.js
@@ -20,3 +20,45 @@ const swiper = new Swiper(".swiper", {
   speed: 400,
   spaceBetween: 100,
 });
+
+const swiper2 = new Swiper(".swiper2", {
+  slidePerView: 3,
+  spaceBetween: 35,
+  slidesPerGroup: 1,
+  loop: true,
+  fade: true,
+  centerSlide: true,
+  grabCursor: true,
+  loopfillGroupwithBlank: true,
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 400,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    depth: 0,
+    modifier: 1,
+    rotate: 0,
+    scale: 0.8,
+    slideShadows: false,
+    stretch: 0,
+  },
+
+  breakpoints: {
+    //when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    //when window width is >= 700px
+    700: {
+      slidesPerView: 2,
+    },
+    //when window width is >= 900px
+    900: {
+      slidesPerView: 3,
+    },
+  },
+});
